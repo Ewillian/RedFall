2ee11614d3052ff489a3068b8f0a1faf2b60065e
paralix0 <florian.lafuente@gmail.com>
2019-12-04 10:02:12 +0100

    base personnage avec equipement + suppression script inconnu

--- RedFall/Assets/Scripts/Items/Equipment/Armor.cs
+++ RedFall/Assets/Scripts/Items/Equipment/Armor.cs
@@ -6,4 +6,11 @@ public class Armor : Equipment
 {
     public ArmorType ArmorType { get; set; }
     public int Protection { get; set; }
+
+    public Armor(string name, ArmorType type, int protection)
+    {
+        this.Name = name;
+        this.ArmorType = type;
+        this.Protection = protection;
+    }
 }
--- RedFall/Assets/Scripts/Items/Equipment/Weapon.cs
+++ RedFall/Assets/Scripts/Items/Equipment/Weapon.cs
@@ -6,4 +6,11 @@ public class Weapon : Equipment
 {
     public WeaponType WeaponType { get; set; }
     public int Damage { get; set; }
+
+    public Weapon(string name, WeaponType type, int damage)
+    {
+        this.Name = name;
+        this.WeaponType = type;
+        this.Damage = damage;
+    }
 }
