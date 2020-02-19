277e789d5dd5023fcc5c5859d07827d426f077cc
Ewillian <guillaume.lecoq@ynov.com>
2020-02-19 15:32:25 +0100

    Functional cinematic transition

---
+++ RedFall/Assets/Scripts/Cinematic/ChangeSceneVideoEnded.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class ChangeSceneVideoEnded : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    void Start()
+    {
+        videoPlayer.Play();
+        StartCoroutine("WaitForMovieEnd");
+    }
+
+
+    public IEnumerator WaitForMovieEnd()
+    {
+        while (!videoPlayer.isPrepared || videoPlayer.isPlaying)
+        {
+            yield return new WaitForEndOfFrame();
+
+        }
+        OnMovieEnded();
+    }
+
+    void OnMovieEnded()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
